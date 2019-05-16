188b594c82b642c6fad264109dbd21cf1b119b29
Ammar Jawad <b-ajawad@hotmail.com>
2019-05-16 04:19:56 +0100

    Finish web app
    feat: Finalised the web app
    
    Add index.html, report of notebook and ipynb file along with train.py and predict.py

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -70,8 +70,8 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
     
-    input_data_cleansed = review_to_words(input_data)
-    data_X, data_len = convert_and_pad(model.word_dict, input_data_cleansed)
+    input_data_cleaned = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, input_data_cleaned)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -89,5 +89,6 @@ def predict_fn(input_data, model):
 
     result = model(data).cpu().detach()
     result = np.array(round(result.item()))
+    
 
     return result
